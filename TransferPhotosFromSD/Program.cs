687ae939e214ff610634aa49ffe122223443abf5
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-05-23 11:40:05 +0100

    First commit, with a console-app that lists filenames grouped by extension

---
+++ TransferPhotosFromSD/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TransferPhotosFromSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groupedFilepaths = GetFilesFromCard()
+                .GroupBy(file => Path.GetExtension(file).ToLower())
+                .OrderBy(group => group.Key);
+            foreach (var group in groupedFilepaths)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(group.Key);
+                Console.ForegroundColor = ConsoleColor.White;
+                foreach (string filepath in group.ToList())
+                {
+                    Console.WriteLine(Path.GetFileName(filepath));
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static string[] GetFilesFromCard()
+        {
+            return Directory.GetFiles(@"D:\", "*", SearchOption.AllDirectories);
+        }
+    }
+}
