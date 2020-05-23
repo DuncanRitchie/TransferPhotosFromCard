3de53f48dc2d2230cc3e4290cd1ba6a0a11932d7
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-05-23 12:30:32 +0100

    Files are grouped by what action should be taken on them

--- TransferPhotosFromSD/Program.cs
+++ TransferPhotosFromSD/Program.cs
@@ -9,10 +9,39 @@ namespace TransferPhotosFromSD
 {
     class Program
     {
+        enum DesiredActionForFileType
+        {
+            AskUser, Ignore, Move, MoveAndRename
+        }
+
+        static DesiredActionForFileType GetDesiredActionForFileType(string filepath)
+        {
+            switch (Path.GetExtension(filepath).ToLower())
+            {
+                case ".jpg":
+                    return DesiredActionForFileType.Move;
+                case ".mts":
+                    return DesiredActionForFileType.MoveAndRename;
+                case "":
+                case ".bdm":
+                case ".bin":
+                case ".bnp":
+                case ".cpi":
+                case ".dat":
+                case ".inp":
+                case ".int":
+                case ".mpl":
+                case ".xml":
+                    return DesiredActionForFileType.Ignore;
+                default:
+                    return DesiredActionForFileType.AskUser;
+            }
+        }
+
         static void Main(string[] args)
         {
             var groupedFilepaths = GetFilesFromCard()
-                .GroupBy(file => Path.GetExtension(file).ToLower())
+                .GroupBy(file => GetDesiredActionForFileType(file))
                 .OrderBy(group => group.Key);
 
             foreach (var group in groupedFilepaths)
@@ -22,43 +51,39 @@ namespace TransferPhotosFromSD
                 Console.ForegroundColor = ConsoleColor.White;
                 switch (group.Key)
                 {
-                    case ".jpg":
+                    case DesiredActionForFileType.Move:
                         Console.ForegroundColor = ConsoleColor.Cyan;
-                        foreach (string filepath in group.ToList())
+                        foreach (string filepath in group)
                         {
                             Console.WriteLine(Path.GetFileName(filepath));
                         }
                         Console.ForegroundColor = ConsoleColor.White;
                         break;
-                    case ".mts":
+                    case DesiredActionForFileType.MoveAndRename:
                         Console.ForegroundColor = ConsoleColor.Cyan;
-                        foreach (string filepath in group.ToList())
+                        foreach (string filepath in group)
                         {
                             Console.WriteLine($"{Path.GetFileName(filepath)} — created {File.GetCreationTime(filepath)}");
                         }
                         Console.ForegroundColor = ConsoleColor.White;
                         break;
-                    case "":
-                    case ".bdm":
-                    case ".bin":
-                    case ".bnp":
-                    case ".cpi":
-                    case ".dat":
-                    case ".inp":
-                    case ".int":
-                    case ".mpl":
-                    case ".xml":
-                        foreach (string filepath in group.ToList())
+                    case DesiredActionForFileType.Ignore:
+                        foreach (string filepath in group)
                         {
-                            Console.WriteLine($"{Path.GetFileName(filepath)} (not worth copying)");
+                            Console.WriteLine(Path.GetFileName(filepath));
                         }
                         break;
-                    default:
+                    case DesiredActionForFileType.AskUser:
                         Console.ForegroundColor = ConsoleColor.Red;
-                        foreach (string filepath in group.ToList())
+                        foreach (string filepath in group)
                         {
                             Console.WriteLine($"{Path.GetFileName(filepath)} (what is this???)");
                         }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Enum no longer matches switch block...");
                         Console.ForegroundColor = ConsoleColor.White;
                         break;
                 }
