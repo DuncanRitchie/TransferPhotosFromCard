08ac2966d0d3fa0480831747f595edbfdfb36008
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2020-05-23 12:18:18 +0100

    Says whether files are worth copying

--- TransferPhotosFromSD/Program.cs
+++ TransferPhotosFromSD/Program.cs
@@ -14,14 +14,53 @@ namespace TransferPhotosFromSD
             var groupedFilepaths = GetFilesFromCard()
                 .GroupBy(file => Path.GetExtension(file).ToLower())
                 .OrderBy(group => group.Key);
+
             foreach (var group in groupedFilepaths)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(group.Key);
                 Console.ForegroundColor = ConsoleColor.White;
-                foreach (string filepath in group.ToList())
+                switch (group.Key)
                 {
-                    Console.WriteLine(Path.GetFileName(filepath));
+                    case ".jpg":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        foreach (string filepath in group.ToList())
+                        {
+                            Console.WriteLine(Path.GetFileName(filepath));
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case ".mts":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        foreach (string filepath in group.ToList())
+                        {
+                            Console.WriteLine($"{Path.GetFileName(filepath)} — created {File.GetCreationTime(filepath)}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case "":
+                    case ".bdm":
+                    case ".bin":
+                    case ".bnp":
+                    case ".cpi":
+                    case ".dat":
+                    case ".inp":
+                    case ".int":
+                    case ".mpl":
+                    case ".xml":
+                        foreach (string filepath in group.ToList())
+                        {
+                            Console.WriteLine($"{Path.GetFileName(filepath)} (not worth copying)");
+                        }
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        foreach (string filepath in group.ToList())
+                        {
+                            Console.WriteLine($"{Path.GetFileName(filepath)} (what is this???)");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
                 }
             }
             Console.ReadLine();
